e4afc990351a9799ad0c9af972b8f34347a8b339
Ron Warholic <ron.warholic@immuta.com>
2022-12-01 22:36:50 -0500

    Day 1

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,11 +1,19 @@
-(ns advent-of-code.day-01)
+(ns advent-of-code.day-01
+  (:require [clojure.string :as str]))
+
+(defn totals [input] (map #(reduce + (map read-string (str/split %1 #"\n"))) (str/split input #"\n\n")))
 
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+
+  (apply max (totals input))
+ )
+
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+
+  (reduce + (take-last 3 (sort (totals input))))
+  )
