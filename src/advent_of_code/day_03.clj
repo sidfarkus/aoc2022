5595b248a8fdd9ef292e7bc2c6535c59da7933f5
Ron Warholic <ron.warholic@immuta.com>
2022-12-03 00:53:25 -0500

    Day 3

--- src/advent_of_code/day_03.clj
+++ src/advent_of_code/day_03.clj
@@ -1,11 +1,26 @@
-(ns advent-of-code.day-03)
+(ns advent-of-code.day-03
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
 
+(defn value [x] (+ (if (< (int x) 97) 27 1) (str/index-of "abcdefghijklmnopqrstuvwxyz" (str/lower-case x))))
 (defn part-1
   "Day 03 Part 1"
   [input]
-  input)
+  (->> input
+       (str/split-lines)
+       (map #(split-at (/ (.length %1) 2) (char-array %1)))
+       (map (fn [[a b]] (first (set/intersection (set a) (set b)))))
+       (map value)
+       (reduce +)))
+
 
 (defn part-2
   "Day 03 Part 2"
   [input]
-  input)
+  (->> input
+       (str/split-lines)
+       (partition 3)
+       (map (fn [[a b c]] (first (set/intersection (set a) (set b) (set c)))))
+       (map value)
+       (reduce +)
+       ))
