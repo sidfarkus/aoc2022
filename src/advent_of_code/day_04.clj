f370268e77c7e76f1b08d0d2b6523775ff2faead
Ron Warholic <ron.warholic@immuta.com>
2022-12-04 17:05:13 -0500

    day 4

--- src/advent_of_code/day_04.clj
+++ src/advent_of_code/day_04.clj
@@ -1,11 +1,21 @@
-(ns advent-of-code.day-04)
+(ns advent-of-code.day-04
+  (:require [clojure.string :as str] [clojure.set :as set]))
 
+(defn irange [start end] (range start (inc end)))
 (defn part-1
   "Day 04 Part 1"
-  [input]
-  input)
+  ([input pred]
+   (->> input
+        (str/split-lines)
+        (map #(str/split % #","))
+        (map (fn [elves] (map #(set (apply irange (map read-string (str/split % #"-")))) elves)))
+        (filter pred)
+        count))
+  ([input]
+   (part-1 input (fn [[elf1 elf2]] (or (set/subset? elf1 elf2) (set/superset? elf1 elf2))))
+   ))
 
 (defn part-2
   "Day 04 Part 2"
   [input]
-  input)
+  (part-1 input (fn [[elf1 elf2]] (seq (set/intersection elf1 elf2)))))
--- test/advent_of_code/day_04_test.clj
+++ test/advent_of_code/day_04_test.clj
@@ -4,9 +4,9 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
+  (let [expected 2]
     (is (= expected (part-1 (slurp (resource "day-04-example.txt")))))))
 
 (deftest part2
-  (let [expected nil]
+  (let [expected 4]
     (is (= expected (part-2 (slurp (resource "day-04-example.txt")))))))
