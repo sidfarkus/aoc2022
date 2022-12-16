1dabdba35e2445b82d879c4c3cb9d7dd1d3931bd
Ron Warholic <blodbath@gmail.com>
2022-12-16 00:41:15 -0500

    Day 12 :oof:

--- src/advent_of_code/day_12.clj
+++ src/advent_of_code/day_12.clj
@@ -1,11 +1,32 @@
-(ns advent-of-code.day-12)
+(ns advent-of-code.day-12 
+  (:require [advent-of-code.util :refer [some-grid grid-get map-grid supersplit tlbr]]
+            [astar.core :as astar]))
 
+(defn reachable? [val other] 
+  (and (some? other)
+   (let [a (cond (= val "S") "a" (= val "E") "z" :else val)
+         b (cond (= other "S") "a" (= other "E") "z" :else other)] 
+       (contains? (set (map #(str (char %)) (range (inc (int (.charAt a 0))) 96 -1))) b))))
+(defn dist [[x1 y1] [x2 y2]]
+  (Math/abs (+ (- x1 x2) (- y1 y2))))
 (defn part-1
   "Day 12 Part 1"
-  [input]
-  )
+  ([input] (count (first (part-1 input #{"S"}))))
+  ([input start-chars] 
+  (let [grid (vec (map vec (supersplit [#"\n" #""] input)))
+        starts (some-grid #(when (contains? start-chars (grid-get grid %1 %2)) [%1 %2]) grid)
+        [end] (some-grid #(when (= (grid-get grid %1 %2) "E") [%1 %2]) grid)
+        graph (apply hash-map
+                     (apply concat (map-grid (fn [x y]
+                         (let [val (grid-get grid x y)
+                               edges (map second
+                                          (filter #(reachable? val (first %))
+                                                  (tlbr grid x y #(vector %1 %2))))]
+                           [[x y] edges])) 
+                        grid)))]
+    (pmap (fn [start] (astar/route graph dist (partial dist end) start end)) starts))))
 
-(defn part-2
+(defn part-2 
   "Day 12 Part 2"
   [input]
-  input)
+  (apply min (filter #(> % 0) (map count (part-1 input #{"S" "a"})))))
--- src/advent_of_code/util.clj
+++ src/advent_of_code/util.clj
@@ -10,7 +10,6 @@
     (map (partial supersplit (rest delimiters) fn) (cs/split to-split (first delimiters)))
     (fn to-split))))
 
-
 (defn make-grid
   ([column-delim str] (make-grid column-delim identity str))
   ([column-delim fn str] (vec (map vec (supersplit [#"\n" column-delim] fn str)))))
@@ -19,19 +18,38 @@
   [width height initial-fn] (vec (map (fn [y] (map #(vec (initial-fn % y)) (range width))) (range height))))
 
 (defn grid-get
-  [grid x y] (get (get grid y) x))
+  ([grid [x y]] (grid-get grid x y))
+  ([grid x y] (get (get grid y) x)))
+
+(defn map-grid [fun grid] 
+  (let [width (count (first grid))
+        height (count grid)]
+    (apply concat (map (fn [y] (map #(fun % y) (range width))) (range height)))))
+
+(defn some-grid [matchfn grid]
+  (filter some? (map-grid matchfn grid)))
+
+(defn print-grid [grid printfn]
+  (let [gridstr (for [y (range (count grid))]
+                      (cs/join "" (for [x (range (count (first grid)))]
+                        (printfn x y))))] 
+    (println (cs/join "\n" gridstr))))
 
 (defn clamp
   ([v min max clamped] (cond (> v max) clamped (< v min) clamped :else v))
   ([v min max] (cond (> v max) max (< v min) min :else v))
   ([v [min max]] (clamp v min max)))
 
-(defn tlbr [grid x y]
+(defn tlbr [grid x y fun]
   (let [width (count (first grid))
-        height (count grid)]
+        height (count grid)
+        t [x (clamp (dec y) 0 height nil)]
+        l [(clamp (dec x) 0 width nil) y]
+        b [x (clamp (inc y) 0 height nil)]
+        r [(clamp (inc x) 0 width nil) y]]
     [
-     (grid-get grid x (clamp (dec y) 0 height nil))
-     (grid-get grid (clamp (dec x) 0 width nil) y)
-     (grid-get grid x (clamp (inc y) 0 height nil))
-     (grid-get grid (clamp (inc x) 0 width nil) y)
+     (fun (grid-get grid t) t)
+     (fun (grid-get grid l) l)
+     (fun (grid-get grid b) b)
+     (fun (grid-get grid r) r)
      ]))
